f54b40cd8cae7345401003f8b7ff399a4f7a65b8
Glitch (starter-cra-and-express) <none>
2023-09-24 14:19:13 +0000

    🐷🐊 Checkpoint ./src/Moviecard.js:61641250/1443 ./package.json:61641250/2114

--- src/Moviecard.js
+++ src/Moviecard.js
@@ -1,20 +1,10 @@
 import React, { useState, useEffect } from "react";
-
 import "./App.css";
 
-function App() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    async function getData() {
-      const response = await fetch("/api/movies");
-      const payload = await response.json();
-      setMovies(payload.data);
-    }
-    getData();
-  }, []);
+function Moviecard({image,title, tagline , vote_average}) {
+
   return (
     <div>
-        
         
     </div>
   );
