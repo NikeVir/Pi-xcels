7a2744a089c883d48508d7f64463bbffa1fca83e
Glitch (starter-cra-and-express) <none>
2023-09-24 14:31:09 +0000

    🔔🍭 Checkpoint ./src/Moviecard.js:61641250/934 ./src/App.js:61641250/863

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+// import Moviecard from "./Moviecard.js"
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     async function getData() {
       const response = await fetch("/api/movies");
       const payload = await response.json();
       setMovies(payload.data);
+      setIsLoading(false);
     }
     getData();
+
   }, []);
   return (
     <div>
@@ -18,8 +21,23 @@ function App() {
          <a className="logo" href="index.html"><span className="mark">GLITCH</span>Movies</a>    
       </nav>
       
-      <ul className="movie_list_ul" id="movieUl"></ul>
+       {isLoading ? (
+        <p>Wait for loading...</p>
+      ) : (
+        movies.length > 0 ? (
+          movies.map((item, index) => (
+            <div key={index}>
+              {JSON.stringify(item)} 
+            </div>
+          ))
+        ) : (
+          <p>No data available.</p>
+        )
+      )}
+    
+      
 
+        
       <footer>
          <p className="copyright-text">&copy; 2023 Copyright All Rights Reserved Designed by <span className="mark">WebsCodeMedia</span></p>
         <p className="copyright-text">This product uses the TMDb API.</p>
