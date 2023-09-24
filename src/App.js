555a593e9c0bb5bc20b284d9877db00acd477e30
Glitch (starter-cra-and-express) <none>
2023-09-24 14:43:09 +0000

    🍀😄 Checkpoint ./src/App.js:61641250/1382

--- src/App.js
+++ src/App.js
@@ -6,38 +6,41 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+   
     async function getData() {
       const response = await fetch("/api/movies");
       const payload = await response.json();
+      console.log("hello")
       setMovies(payload.data);
       setIsLoading(false);
     }
+     setTimeout(() => {
     getData();
-
+ }, 2000);
+    
   }, []);
   return (
     <div>
       <nav>
          <a className="logo" href="index.html"><span className="mark">GLITCH</span>Movies</a>    
       </nav>
-      
+    
        {isLoading ? (
         <p>Wait for loading...</p>
       ) : (
-        movies.length > 0 ? (
-          movies.map((item, index) => (
-            <div key={index}>
-              {JSON.stringify(item)} 
-            </div>
-          ))
-        ) : (
-          <p>No data available.</p>
-        )
-      )}
-    
-      
-
-        
+            <div style={{background:"black"}}>{movies}</div>
+        // movies.length > 0 ? (
+        //   movies.map((item, index) => (
+        //     <div key={index}>
+        //       {JSON.stringify(item)} 
+        //     </div>
+        //   ))
+        ) 
+        // : (
+        //   <p>No data available.</p>
+        // )
+      }
+ 
       <footer>
          <p className="copyright-text">&copy; 2023 Copyright All Rights Reserved Designed by <span className="mark">WebsCodeMedia</span></p>
         <p className="copyright-text">This product uses the TMDb API.</p>
@@ -47,16 +50,3 @@ function App() {
 }
 
 export default App;
-        // <img src={logo} className="App-logo" alt="logo" />
-        // <p>
-        //   Edit <code>src/App.js</code> and your changes will live-update
-        //   automatically.
-        // </p>
-        // <a
-        //   className="App-link"
-        //   href="https://reactjs.org"
-        //   target="_blank"
-        //   rel="noopener noreferrer"
-        // >
-        //   Learn React
-        // </a>
