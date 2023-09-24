1476541fe67ba6202fde6f4da20b1ff5f5a1e850
Glitch (starter-cra-and-express) <none>
2023-09-24 13:21:09 +0000

    🌛📕 Checkpoint ./server/server.js:61641250/319

--- server/server.js
+++ server/server.js
@@ -8,11 +8,11 @@ app.get("/api/ping", (request, response) => {
   console.log("❇️ Received GET request to /api/ping");
   response.send("pong!");
 });
+const moviesData = require('./movies.json');
 
-// A mock route to return some data.
 app.get("/api/movies", (request, response) => {
-  console.log("❇️ Received GET request to /api/movies");
-  response.json({ data: [{ id: 1, name: '1' }, { id: 2, name: '2' }] });
+  console.log(moviesData);
+  response.json(moviesData);
 });
 
 
@@ -20,7 +20,7 @@ app.get("/api/movies", (request, response) => {
 // Express port-switching logic
 let port;
 console.log("❇️ NODE_ENV is", process.env.NODE_ENV);
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
   port = process.env.PORT || 3000;
   app.use(express.static(path.join(__dirname, "../build")));
   app.get("*", (request, response) => {
